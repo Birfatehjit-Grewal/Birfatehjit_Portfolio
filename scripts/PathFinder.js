2b4861518e2e0a34a761409cf75cb2566dd50b73
Birfatehjit Grewal <Birfateh12@gmail.com>
2024-01-02 15:45:19 -0800

    firbase start setup

--- scripts/PathFinder.js
+++ scripts/PathFinder.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "/node_modules/firebase/app";
-import { getDatabase, ref, set, push, onValue } from "/node_modules/firebase/database";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getDatabase, ref, set, push, onValue } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries

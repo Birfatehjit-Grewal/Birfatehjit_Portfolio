5f7a73ba9545abd24e714d938bf4cb3ed27a7989
Birfatehjit Grewal <Birfateh12@gmail.com>
2024-01-02 15:18:27 -0800

    firbase start setup

--- scripts/PathFinder.js
+++ scripts/PathFinder.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getDatabase, ref, set, push, onValue } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/latest/firebase-app.esm.js";
+import { getDatabase, ref, set, push, onValue } from "https://www.gstatic.com/firebasejs/latest/firebase-database.esm.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
